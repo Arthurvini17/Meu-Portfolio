3044f64a5e1835fbfba5bfac77cec24249a4cd2d
arthur <arthurdedsec@gmail.com>
2025-04-23 10:30:03 -0300

    iniciando projeto, tudo configurado

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+export const metadata = {
+    title: "Sobre mim | Arthur",
+    description: "Sobre mim",
+};
+
+export default function about() {
+    return (
+        <div>Pagina about</div>
+    )
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,21 @@
+
+import Link from "next/link";
+
+
+
+export default function NotFound() {
+    return (
+        <>
+
+
+            <main>
+                <h1>404</h1>
+                <div>
+                    <p>Oops, Pagina não encotrada</p>
+                    <h1>Clique no botão abaixo para ser rederecionado</h1>
+                    <Link href={'/'}></Link>
+                </div>
+            </main>
+        </>
+    );
+}
