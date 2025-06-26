68468702a31d0b052bdb3a3e170fa38e286964a3
Arthur <arthurdedsec@gmail.com>
2025-06-26 11:06:16 -0300

    style: created new view for about-me

---
+++ src/app/components/About.tsx
@@ -0,0 +1,39 @@
+export default function About() {
+  return (
+    <div className="p-5  flex flex-col  ">
+      <h1 className="text-center text-xl mb-2">Sobre Mim</h1>
+      <p className="  leading-relaxed">
+        Olá! Eu sou <strong>arthur Vinicius</strong>, um Desenvolvedor Full
+        Stack apaixonado por criar experiências digitais incríveis.
+      </p>
+      <p>
+        Tenho <strong>2 anos de experiência</strong> com{" "}
+        <strong>Sistemas Full Stack</strong> e <strong>Banco de Dados</strong>,
+        focando em soluções eficientes que impactam positivamente os usuários.
+      </p>
+      <p>
+        Minha jornada na tecnologia começou cedo — aos <strong>7 anos</strong>{" "}
+        já me interessava por como os computadores funcionavam. Desde então,
+        venho evoluindo com:
+      </p>
+      <ul>
+        <li>
+          <strong>React</strong> & <strong>Next.js</strong>
+        </li>
+        <li>
+          <strong>Laravel</strong>
+        </li>
+        <li>
+          <strong>MySQL</strong>
+        </li>
+        <li>
+          <strong>Livewire</strong>
+        </li>
+      </ul>
+      <p>
+        Estou sempre aprendendo e buscando novas tecnologias para entregar
+        melhores resultados e me manter atualizado com o mercado.
+      </p>
+    </div>
+  );
+}
