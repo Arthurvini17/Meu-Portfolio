75247e304bc16f8be850d77a8c9954d35c30c39d
Arthur <arthurdedsec@gmail.com>
2025-06-26 10:24:59 -0300

    style: new view from hero section

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,15 +1,13 @@
 import Image from "next/image";
 export default function Hero() {
     return (
-        <main className="flex items-center justify-center mt-20 ">
-            <div className="flex flex-col items-center text-white">
-                <div className="text-xl text-center">
-                    <h1>Olá, Sejam Bem-Vindos!</h1>
-                    <h1> <span className="font-bold text-[#3BA9F4] "> {'$this->name="Arthur Vinicius";'} </span></h1>
-                    <p>Dev Full Stack</p>
-                </div>
-                <div className="">
-                    <Image src="/images/IMAGEM-CHAT.png" alt="imagem" width={400} height={100} />
+        <main className="flex flex-col  p-5 bg-gradient-to-r from-blue-500 to-indigo-600 h-full w-full ">
+            <div className="flex items-center flex-col text-center   ">
+                <Image src="/images/IMAGEM2-CHAT.png" alt="imagem" className="ml-10" width={150} height={100} />
+                <div className="text-white text-xl">
+                    <h1 className="">Olá, eu sou Arthur!</h1>
+                    <p className="text-md">Desenvolvedor Full Stack| MySQL</p>
+                    <button className="bg-white rounded-md py-2 px-2 text-xs font-bold text-blue-500  mt-2">Ver Meus Projetos</button>
                 </div>
             </div>
         </main>
