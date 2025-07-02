2a6c3c57dbb8ca199c7b22758809b80c20d867f4
arthur <arthurdedsec@gmail.com>
2025-07-02 10:45:31 -0300

    style fix: Alteração na descrição do projeto

--- src/app/components/Project-Card.tsx
+++ src/app/components/Project-Card.tsx
@@ -16,7 +16,7 @@ const poppins = Poppins({
 export default function ProjectsCards() {
   return (
     <div className={`${poppins.className} p-2 bg-slate-50`}>
-      <h1 className="text-center mb-4">Meus Projetos</h1>
+      <h1 className="mb-4 text-center">Meus Projetos</h1>
 
       <Swiper
         modules={[Navigation]}
@@ -28,40 +28,55 @@ export default function ProjectsCards() {
       >
         {/* Slide 1 - HostPool */}
         <SwiperSlide>
-          <div className="bg-white shadow w-full h-full p-2">
+          <div className="w-full h-full p-2 shadow bg-slate-50">
             <div className="h-[100px] w-full overflow-hidden rounded-t-lg">
               <Image
                 src="/images/hostpool.png"
                 alt="imagemhost"
-                className="w-full h-full object-cover"
+                className="object-cover w-full h-full"
                 width={400}
                 height={200}
               />
             </div>
-            <div className="p-2 flex flex-col gap-2 ">
+            <div className="flex flex-col gap-2 p-2 ">
               <h1 className="font-bold">HostPool</h1>
               <p className="text-xs leading-relaxed">
-                Uma breve descrição do projeto, destacando o que ele faz e as
-                principais tecnologias utilizadas
+                Plataforma de hospedagem que permite criar ambientes de desenvolvimento usando Laravel e
+                Docker. O projeto possui painel administrativo completo para gerenciamento de instâncias.
               </p>
-              <p className="text-xs">Laravel Livewire Tailwind</p>
+              <p className="text-xs">Laravel | Docker | MySQL | Livewire |
+                Alpine JS </p>
+              <div className="flex gap-4 text-xs">
+                <a
+                  href="https://tech-product-lp.vercel.app/"
+                  className="font-semibold text-blue-500 underline"
+                >
+                  Projeto não hospedado
+                </a>
+                <a
+                  href="https://github.com/Arthurvini17/HostPool"
+                  className="flex items-center gap-1 text-gray-800 hover:text-black"
+                >
+                  GitHub <FaGithub />
+                </a>
+              </div>
             </div>
           </div>
         </SwiperSlide>
 
         {/* Slide 2 - Tech-Product-LP */}
         <SwiperSlide>
-          <div className="bg-white shadow">
+          <div className="shadow bg-slate-50">
             <div className="h-[200px] overflow-hidden rounded-t-lg">
               <Image
                 src="/images/techpc2.png"
                 alt="imagem"
-                className="w-full h-full object-cover"
+                className="object-cover w-full h-full"
                 width={400}
                 height={200}
               />
             </div>
-            <div className="p-2 flex flex-col gap-2">
+            <div className="flex flex-col gap-2 p-2">
               <h1 className="font-bold">Tech-Product-LP</h1>
               <p className="text-xs leading-relaxed">
                 Landing page inspirada usando I.A da Gemini para fins de
@@ -72,7 +87,7 @@ export default function ProjectsCards() {
               <div className="flex gap-4 text-xs">
                 <a
                   href="https://tech-product-lp.vercel.app/"
-                  className="underline text-blue-500 font-semibold"
+                  className="font-semibold text-blue-500 underline"
                 >
                   Visualizar
                 </a>
