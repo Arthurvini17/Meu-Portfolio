86893cbdf6e27b79a0644f7b0a7ebd14e0e0d99f
arthur <arthurdedsec@gmail.com>
2025-07-02 11:56:30 -0300

    Alteração da imagem no hero section

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 export default function Hero() {
     return (
-        <main className="flex flex-col  p-5 bg-gradient-to-r from-blue-500 to-indigo-600 h-full w-full ">
-            <div className="flex items-center flex-col text-center   ">
-                <Image src="/images/IMAGEM2-CHAT.png" alt="imagem" className="ml-10" width={150} height={100} />
-                <div className="text-white text-xl">
+        <main className="flex flex-col w-full h-full p-5 bg-gradient-to-r from-blue-500 to-indigo-600 ">
+            <div className="flex flex-col items-center text-center ">
+                <Image src="/images/imagem-eu.jpg" alt="imagem" className="ml-10 text-center rounded-lg " width={150} height={100} />
+                <div className="text-xl text-white">
                     <h1 className="">Olá, eu sou Arthur!</h1>
-                    <p className="text-md">Desenvolvedor Full Stack| MySQL</p>
-                    <button className="bg-white rounded-md py-2 px-2 text-xs font-bold text-blue-500  mt-2">Ver Meus Projetos</button>
+                    <p className="text-md">Desenvolvedor Full Stack | MySQL</p>
+                    <button className="px-2 py-2 mt-2 text-xs font-bold text-blue-500 bg-white rounded-md">Ver Meus Projetos</button>
                 </div>
             </div>
         </main>
