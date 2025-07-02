d05ea79dfffc2b0ccd555bd26f927ae0b8442b0e
arthur <arthurdedsec@gmail.com>
2025-07-02 11:16:21 -0300

    Styles: Criação do componente Contact

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Hero from "./components/Hero";
 import ProjectsCards from "./components/Project-Card";
 import Stacks from "./components/Stacks";
@@ -15,6 +16,7 @@ export default function Home() {
       <About />
       <Stacks />
       <ProjectsCards />
+      <Contact />
     </>
   );
 }
