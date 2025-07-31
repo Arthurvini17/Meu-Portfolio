cdb2b2169a2cef738b722479196b235c92c9ea6c
arthur <arthurdedsec@gmail.com>
2025-07-31 12:11:18 -0300

    Style: hero-section responsivdade feita

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 export default function Hero() {
     return (
         <main className="flex flex-col w-full h-full p-5 bg-gradient-to-r from-blue-500 to-indigo-600 ">
-            <div className="flex flex-col items-center text-center ">
+            <div className="flex flex-col items-center text-center md:flex md:flex-row md:justify-center md:gap-5">
                 <Image src="/images/imagem-eu.jpg" alt="imagem" className="ml-10 text-center rounded-lg " width={150} height={100} />
                 <div className="text-xl text-white">
-                    <h1 className="">Olá, eu sou Arthur!</h1>
-                    <p className="text-md">Desenvolvedor Full Stack | MySQL</p>
+                    <h1 className="md:text-2xl">Olá, eu sou Arthur!</h1>
+                    <p className="md:text-xl text-md">Desenvolvedor Full Stack | MySQL</p>
                     <Link href={"#projects"}>
-                        <button className="px-2 py-2 mt-2 text-xs font-bold text-blue-500 bg-white rounded-md">Ver Meus Projetos</button>
+                        <button className="px-2 py-2 mt-2 text-xs font-bold text-blue-500 bg-white rounded-md hover:cursor-pointer md:text-md">Ver Meus Projetos</button>
                     </Link>
                 </div>
             </div>
